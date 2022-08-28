5601825eba07285bc85f1da36ae327ef0a239809
Unaisul Hadi <unaisulhadi@gmail.com>
2022-08-28 18:22:33 +0530

    Added splash images, Set up text style, Added fonts, Splash Screen,

--- app/src/main/java/com/hadi/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/hadi/newsapp/presentation/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.hadi.newsapp.presentation
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.hadi.newsapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +13,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //hideStatusBar()
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun hideStatusBar() {
+        if (Build.VERSION.SDK_INT < 30) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            actionBar?.hide()
+        } else {
+            window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars())
+        }
     }
 
 
