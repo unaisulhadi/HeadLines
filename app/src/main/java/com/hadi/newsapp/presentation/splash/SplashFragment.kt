7388ba9bbfcdb6aa4b257c700882c77534ca44ef
Unaisul Hadi <unaisulhadi@gmail.com>
2022-08-29 14:49:20 +0530

    Set up Home in Prgress

--- app/src/main/java/com/hadi/newsapp/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/hadi/newsapp/presentation/splash/SplashFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hadi.newsapp.R
+import com.hadi.newsapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding ?= null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater,container, false)
+
+        binding.btnStart.setOnClickListener{
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
+
+        return binding.root
     }
 
 }
