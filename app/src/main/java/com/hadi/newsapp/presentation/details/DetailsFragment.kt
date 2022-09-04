bc8555187d5c2392a758a0bdb397e7dd811b7142
Unaisul Hadi <unaisulhadi@gmail.com>
2022-09-04 22:18:45 +0530

    Fixed unresolved type - nav args

--- app/src/main/java/com/hadi/newsapp/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/hadi/newsapp/presentation/details/DetailsFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.hadi.newsapp.R
+import com.hadi.newsapp.data.model.NewsResponse
+import com.hadi.newsapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private val args by navArgs<DetailsFragmentArgs>()
+
+    private lateinit var article: NewsResponse.Article
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+
+        initialize()
+
+        return binding.root
+    }
+
+    private fun initialize() {
+        article = args.newsArticle
+        binding.tvNewsTitle.text = article.title
+        binding.tvContent.text = article.content
+        binding.ivThumbnail.load(article.urlToImage)
     }
 
 }
